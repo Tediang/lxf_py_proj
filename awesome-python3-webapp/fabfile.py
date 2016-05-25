1b45fc91b01fb29927c57c10541296c627358da7
ted <tediang920@gmail.com>
2016-05-26 07:41:11 +0800

    fabfile finish

--- awesome-python3-webapp/fabfile.py
+++ awesome-python3-webapp/fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import *
 
 env.user = 'root'
 env.sudo_user = 'root'
-env.hosts = ['192.168.0.112']
+env.hosts = ['192.168.0.152']
 
 db_users = 'root'
 db_password = 'centerm'
@@ -21,3 +21,29 @@ def build():
         cmd.extend(['--exclude=\'%s\'' % ex for ex in excludes])
         cmd.extend(includes)
         local(' '.join(cmd))
+
+
+_REMOTE_TMP_TAR = '/tmp/%s' % _TAR_FILE
+_REMOTE_BASE_DIR = '/srv/awesome'
+
+def deploy():
+    newdir = 'www-%s' %datetime.now().strftime('%y-%m-%d_%H.%M.%S')
+    run('rm -f %s' % _REMOTE_TMP_TAR)
+    put('dist/%s' % _TAR_FILE, _REMOTE_TMP_TAR)
+    with cd(_REMOTE_BASE_DIR):
+        sudo('mkdir %s' % newdir)
+
+    with cd('%s/%s' % (_REMOTE_BASE_DIR, newdir)):
+        sudo('tar -zxvf %s' % _REMOTE_TMP_TAR)
+
+    with cd(_REMOTE_BASE_DIR):
+        sudo('rm -rf www')
+        sudo('ln -s %s www' % newdir)
+        sudo('chmod +x /srv/awesome/www/app.py')
+        # sudo('chown www-data:www-data www')
+        # sudo('chown -R www-data:www-data %s' % newdir)
+
+    with settings(warn_only=True):
+        sudo('supervisorctl stop awesome')
+        sudo('supervisorctl start awesome')
+        sudo('nginx -s reload')
